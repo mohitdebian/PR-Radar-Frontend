dd87611f436ac3745632e60580e393c1ad7fb1b3
Mohit Singh <mohitdebian@gmail.com>
2026-02-23 16:03:00 +0530

    Cleanup unused import

--- src/components/IssueCard.tsx
+++ src/components/IssueCard.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import type { ScoredIssue, Classification } from "../types";
+import type { ScoredIssue } from "../types";
 
 interface Props {
     issue: ScoredIssue;
